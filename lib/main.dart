ea80e4523937bd32fcac659ab36fd8ca3a3c68f8
Ehmad <justEhmadSaeed@gmail.com>
2020-06-15 11:14:30 +0500

    👌 IMPROVE: integrate TasksScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/tasks_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }

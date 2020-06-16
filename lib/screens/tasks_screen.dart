eb70b587a5df78b5855c47d2c5af6a098cb8980c
Ehmad <justEhmadSaeed@gmail.com>
2020-06-16 09:01:50 +0500

    📦 NEW: Add icon & container

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,6 +3,57 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: null),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.lightBlueAccent,
+                      size: 30,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'To-Do it',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    '12 Tasks',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ]),
+          ),
+            Expanded(
+                child: Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(30),
+                          topLeft: Radius.circular(30))),
+                ),
+              )
+        ],
+      ),
+    );
   }
 }

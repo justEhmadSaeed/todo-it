cde2887f6375106b38eb7ce750a83a6c8d9f4ee7
Ehmad Saeed <justehmadsaeed@gmail.com>
2020-10-05 11:57:44 +0500

    📦 NEW: Local Todos storage functionality

--- lib/models/task-data.dart
+++ lib/models/task-data.dart
@@ -2,20 +2,43 @@ import 'package:flutter/cupertino.dart';
 import "package:flutter/foundation.dart";
 import 'package:todo_app/models/task.dart';
 import 'dart:collection';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> _tasks = [
-    Task(name: 'Buy Milk'),
-    Task(name: 'Buy Eggs'),
-    Task(name: 'Buy Books'),
-  ];
+  TaskData() {
+    readPref();
+  }
+  List<Task> _tasks = [];
+
+  void _writePref(String taskTitle, bool isChecked) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool(taskTitle, isChecked);
+  }
+
+  void readPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    final keys = prefs.getKeys() ?? null;
+    if (keys == null) return;
+
+    for (String key in keys) {
+      _tasks.add(Task(name: key, isDone: prefs.get(key)));
+    }
+    notifyListeners();
+  }
+
   UnmodifiableListView<Task> get tasks => UnmodifiableListView(_tasks);
   int get taskCount => _tasks.length;
 
   void addTask(String taskTitle) {
     if (taskTitle?.isEmpty ?? true) return;
 
+    var existingTask = _tasks.firstWhere((element) => element.name == taskTitle,
+        orElse: () => null);
+
+    if (existingTask != null) return;
+
     _tasks.add(Task(name: taskTitle));
+    _writePref(taskTitle, false);
     notifyListeners();
   }
 
@@ -24,8 +47,9 @@ class TaskData extends ChangeNotifier {
     notifyListeners();
   }
 
-  void toggleCheckBox(task) {
+  void toggleCheckBox(Task task) {
     task.toggleDone();
+    _writePref(task.name, task.isDone);
     notifyListeners();
   }
 }
